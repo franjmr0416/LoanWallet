45c4f0dc371fb061aed94620bed06abd99ab8c5c
franjmr0416 <franjmr16@gmail.com>
2022-10-15 15:50:58 -0500

    feat decript password #16

--- src/services/encriptado.js
+++ src/services/encriptado.js
@@ -1,11 +1,18 @@
 const bcrypt = require('bcrypt');
 
+const SALT = 10;
+
 const passEncryptService = async (password) => {
-  const SALT = 10;
   const hash = await bcrypt.hash(password, SALT);
   return hash;
 };
 
+const passDecryptService = async (hashPassword) => {
+  const compare = await bcrypt.compare(hashPassword, SALT);
+  return compare;
+};
+
 module.exports = {
   passEncryptService,
+  passDecryptService,
 };
